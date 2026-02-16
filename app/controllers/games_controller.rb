8a686c7d5a3bc3369f23c435f021c78fe761f2a5
Steven Sawadisavi <stevesavi+lewagon@gmail.com>
2026-02-16 16:15:11 +0900

    refactored to move comparison code into ruby file from erb

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,12 +15,22 @@ class GamesController < ApplicationController
   def score
     require "json"
     require "open-uri"
-    @letters = params[:letters].split(",")
-    @guess = params[:guess].strip.upcase
+    letters = params[:letters].split(",")
+    guess = params[:guess].strip.upcase
     @score = session[:score]
-    @url = "https://dictionary.lewagon.com/#{@guess}"
-    @valid = @guess.chars.all? do |letter|
-      @guess.chars.count(letter) <= @letters.count(letter)
+    url = "https://dictionary.lewagon.com/#{guess}"
+    valid = guess.chars.all? do |letter|
+      guess.chars.count(letter) <= letters.count(letter)
+    end
+    if !valid
+      @message = { start: "Sorry but ", strong: guess, end: " can't be built out of #{letters.join(", ")}. Your score has been reset." }
+      session[:score] = 0
+    elsif JSON.parse(URI.parse(url).read)["found"]
+      @message = { start: "", strong: "Congratulations! ", end: "#{guess} is a valid English word!" }
+      session[:score] += guess.length
+    else
+      @message = { start: "Sorry but ", strong: guess, end: " does not seem to be a valid English word... Your score has been reset." }
+      session[:score] = 0
     end
   end
 end
