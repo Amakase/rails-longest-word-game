3cf3db1f94ea0592cf5ed278b4a6bb0f44c23bfc
Steven Sawadisavi <stevesavi+lewagon@gmail.com>
2026-02-16 15:51:28 +0900

    implemented total score across multiple rounds

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,13 +7,17 @@ class GamesController < ApplicationController
         @letters << ("A".."Z").to_a.sample(1)[0]
       end
     end
+    if session[:score].nil?
+      session[:score] = 0
+    end
   end
 
   def score
     require "json"
     require "open-uri"
-    @letters = params[:letters].split(", ")
+    @letters = params[:letters].split(",")
     @guess = params[:guess].strip.upcase
+    @score = session[:score]
     @url = "https://dictionary.lewagon.com/#{@guess}"
     @valid = @guess.chars.all? do |letter|
       @guess.chars.count(letter) <= @letters.count(letter)
